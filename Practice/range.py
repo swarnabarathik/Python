8477b58e0ea72671c9f312b10239becf4beb6b9c
swarnabarathik <34641522+swarnabarathik@users.noreply.github.com>
2017-12-26 15:49:00 +0530

    Create range.py

---
+++ Practice/range.py
@@ -0,0 +1,9 @@
+for value in range(1,6):
+    print(value)
+for value in range(0,6,2):
+    print(value)
+for value in range(6,1,-2):
+    print(value)
+for ch in 'Hello world':
+    print(ch.upper())
+    
