a7c59fc352f830d7e2882d2bf6563b807f9f9196
swarnabarathik <34641522+swarnabarathik@users.noreply.github.com>
2017-12-26 15:46:43 +0530

    Create user input.py

---
+++ Practice/user input.py
@@ -0,0 +1,9 @@
+name=input("Enter your name")
+print("Name is:",name)
+num1=int(input("Enter the number"))
+num2=int(input("Enter the number"))
+total=num1+num2
+print("total=",total)
+x=3+0.9j
+print(x.real)
+print(x.imag+3)
