40f11930416dc8345991bc13ce0fa6f6ea020cfc
swarnabarathik <34641522+swarnabarathik@users.noreply.github.com>
2017-12-27 22:47:57 +0530

    Create if else.py

---
+++ Assignment/if else.py
@@ -0,0 +1,15 @@
+employee_id=int(input("enter the employee id"))
+basic_salary=int(input("enter the basic salary"))
+allowance=int(input("enter the allowances"))
+monthly_gross_salary=basic_salary+allowance
+if(monthly_gross_salary>10000):
+	income_tax=20/100*monthly_gross_salary
+	net_salary=monthly_gross_salary-income_tax
+	print("Net salary:%d"%income_tax)
+else:
+	net_salary=monthly_gross_salary
+print("Employee id:%d"%employee_id)
+print("Basic salary:%d"%basic_salary)
+print("Aloowance:%d"%allowance)
+print("gross pay:%d"%monthly_gross_salary)
+print("Net pay:%d"%net_salary)
