5a7bea49f4b6cce049ed820078811360eb8c5b18
swarnabarathik <34641522+swarnabarathik@users.noreply.github.com>
2017-12-27 21:56:39 +0530

    Create operator.py

---
+++ Assignment/operator.py
@@ -0,0 +1,9 @@
+hours_worked=40
+pay_rate=400
+weeks_per_month=4
+monthly_pay=hours_worked*pay_rate*weeks_per_month
+print("EMPLOYEE DETAILS:")
+print("Number of hours worked by the employee in a week:%d"%hours_worked)
+print("Pay rate per hour:Rs %d"%pay_rate)
+print("Number of weeks in a month:%d"%weeks_per_month)
+print("Monthly pay of the employee:%d"%monthly_pay)
