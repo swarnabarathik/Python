0d22a849185f517b07ba7a16e984d536baffe270
swarnabarathik <34641522+swarnabarathik@users.noreply.github.com>
2017-12-28 16:40:48 +0530

    Create occurence.py

---
+++ Assignment/occurence.py
@@ -0,0 +1,12 @@
+def char_frequency(str1):
+    dict={}
+    for n in str1:
+        keys = dict.keys()
+        if n in keys:
+            dict[n] += 1
+        else:
+            dict[n] = 1
+    return dict
+string="ABaBCbGc"
+string=string.upper()
+print(char_frequency(string))
